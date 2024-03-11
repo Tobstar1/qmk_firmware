2971fa388b5527a58ee673b77586ebd8db6c0b07
Tobstar1 <Tobstar1@users.noreply.github.com>
2024-03-11 22:10:50 +0100

    added sofle choc keymap from brianlow branch choc3

--- keyboards/sofle_choc/keymaps/via/rules.mk
+++ keyboards/sofle_choc/keymaps/via/rules.mk
@@ -1,7 +1,18 @@
+ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
-VIA_ENABLE = yes
 
-# Reduce compiled size
-MOUSEKEY_ENABLE = no
-SPACE_CADET_ENABLE = no
-GRAVE_ESC_ENABLE = no
+OLED_ENABLE = yes
+
+# Enable per-key lighting
+RGB_MATRIX_ENABLE = yes
+
+# Enable audio keys and system control
+EXTRAKEY_ENABLE = yes
+
+# Enable link time optimization
+# Trade longer compile time for smaller firmware size
+LTO_ENABLE = yes
+
+# Change keymaps without flashing
+# https://caniusevia.com/
+VIA_ENABLE = yes
